2ef7d33f904e71ccaa08fb2d5073bd4aef3a5f33
sara <sara.drimmer@gmail.com>
2022-04-28 13:37:20 -0400

    trying again

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const db = require('./model/index');
 app.use(cors());
 app.use(express.json());
 app.use(router);
-
+//making another commit
 (async () => {
     try {
         await db.sequelize.sync({force:true});
